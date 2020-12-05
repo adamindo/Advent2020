a6630b51c060a5cde5f09b60437ada9eab5fdb41
Sven <sven@svenideapadz510.localdomain>
2020-12-05 14:53:10 +0100

    TripletFinder

--- day1/ReportRepair/Program.cs
+++ day1/ReportRepair/Program.cs
@@ -11,9 +11,12 @@ namespace ReportRepair
             var loader = new DataLoader("input.txt");
             var sortedNumbers = loader.Numbers;
             sortedNumbers.Sort();
-            var finder = new FindTuple(sortedNumbers);
-            var pair = finder.Find(2020);
+            var pariFinder = new FindTuple(sortedNumbers);
+            var pair = pariFinder.Find(2020);
             Console.WriteLine(pair.Item1 * pair.Item2);
+            var tripletFinder = new FindTriplet(sortedNumbers);
+            var triplet = tripletFinder.Find(2020);
+            Console.WriteLine(triplet.Item1 * triplet.Item2 * triplet.Item3);
         }
     }
 }
