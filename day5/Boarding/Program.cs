7e319223fe8b441e99e4436c85aa1abd95d0ceec
Sven <sven@svenideapadz510.localdomain>
2020-12-06 13:29:43 +0100

    part2

--- day5/Boarding/Program.cs
+++ day5/Boarding/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace Boarding
 {
@@ -9,7 +10,20 @@ namespace Boarding
         {
             var loader = new DataLoader("input.txt");
             var passes = loader.Passes;
-            Console.WriteLine("Part1: " + passes.Max(pass => pass.Id));
+            var maxID = passes.Max(pass => pass.Id);
+            Console.WriteLine("Part1: " + maxID);
+            var emptySeats = new List<int>();
+            for (int i = 1; i < maxID; i++)
+            {
+                if (passes.Any(pass => pass.Id == i))
+                    continue;
+                if (!passes.Any(pass => pass.Id == i + 1))
+                    continue;
+                if (!passes.Any(pass => pass.Id == i - 1))
+                    continue;
+                emptySeats.Add(i);
+            }
+            Console.WriteLine($"Part2: Found {emptySeats.Count()} empty seats: {emptySeats.First()}");
         }
     }
 }
